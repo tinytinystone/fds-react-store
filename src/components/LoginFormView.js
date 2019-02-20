e0023dd8d074b3e40b3bb8c9bff725e5d400bf75
Hong Jeesoo <jeesoo.hong@gmail.com>
2019-02-20 17:15:20 +0900

    내부 state 삭제

--- src/components/LoginFormView.js
+++ src/components/LoginFormView.js
@@ -3,58 +3,23 @@ import { Link, Redirect } from 'react-router-dom';
 import s from './LoginFormView.module.scss';
 
 export default class LoginForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      usernameForLogin: 'ID',
-      passwordForLogin: 'PASSWORD',
-      loginSuccess: false,
-    };
-  }
   handleSubmit = async e => {
-    const { usernameForLogin, passwordForLogin } = this.state;
     e.preventDefault();
+    const usernameForLogin = e.target.elements.username.value;
+    const passwordForLogin = e.target.elements.password.value;
+
     await this.props.onLogin(usernameForLogin, passwordForLogin);
-    // 로그인이 성공적으로 끝났을 때
-    // Redirect 컴포넌트를 렌더링 -> 주소표시줄 상태 바꿈
-    this.setState({
-      loginSuccess: true,
-    });
-    // 로그인에 실패했을 때
-  };
-  handleUNChange = e => {
-    this.setState({
-      usernameForLogin: e.target.value,
-    });
-  };
-  handlePWChange = e => {
-    this.setState({
-      passwordForLogin: e.target.value,
-    });
+
+    alert(`${usernameForLogin} 님 로그인에 성공했습니다`);
+
+    return this.props.history.push('/');
   };
   render() {
     return (
       <React.Fragment>
-        <form
-          onSubmit={e => {
-            this.handleSubmit(e);
-            alert(`${this.state.usernameForLogin} 님 로그인에 성공했습니다`);
-            this.props.history.push('/');
-          }}
-          className={s.loginForm}
-        >
-          <input
-            type="text"
-            name="username"
-            onChange={this.handleUNChange}
-            value={this.state.usernameForLogin}
-          />
-          <input
-            type="password"
-            onChange={this.handlePWChange}
-            value={this.state.passwordForLogin}
-            name="password"
-          />
+        <form onSubmit={this.handleSubmit} className={s.loginForm}>
+          <input type="text" name="username" placeholder="username" />
+          <input type="password" name="password" placeholder="password" />
           <button>로그인</button>
         </form>
         <div>
