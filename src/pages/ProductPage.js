d0cea9aa6032b0f78c8216e9d94d2d7726c6c7df
Hong Jeesoo <jeesoo.hong@gmail.com>
2019-03-06 16:52:34 +0900

    필요 없는 prop 삭제

--- src/pages/ProductPage.js
+++ src/pages/ProductPage.js
@@ -4,11 +4,9 @@ import Layout from '../components/Layout';
 
 export default class ProductPage extends Component {
   render() {
-    const { match } = this.props;
-    const productId = match.params.productId;
     return (
       <Layout>
-        <ProductDetail productId={productId} />
+        <ProductDetail />
       </Layout>
     );
   }
