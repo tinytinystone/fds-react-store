d0933420f5c68e621b332bf6dd2e2bfcdc143fab
Hong Jeesoo <jeesoo.hong@gmail.com>
2019-03-27 00:15:23 +0900

    loading indicator 추가

--- src/components/ProductListView.js
+++ src/components/ProductListView.js
@@ -6,27 +6,36 @@ import withLoading from '../hoc/withLoading';
 import ProductItem from './ProductItem';
 import ProductListPage from './ProductListPage';
 
-const ProductListView = ({ productList, currentCategory, pageNumbers }) => {
-  return (
-    <React.Fragment>
-      <section className={s.list}>
-        {productList.map(product => {
-          return <ProductItem product={product} key={product.id} />;
-        })}
-      </section>
-      <ul className={s.pageNumbers}>
-        {pageNumbers.map(number => {
-          return (
-            <ProductListPage
-              key={number}
-              number={number}
-              currentCategory={currentCategory}
-            />
-          );
-        })}
-      </ul>
-    </React.Fragment>
-  );
+const ProductListView = ({
+  productList,
+  currentCategory,
+  pageNumbers,
+  isFetching,
+}) => {
+  if (isFetching) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <React.Fragment>
+        <section className={s.list}>
+          {productList.map(product => {
+            return <ProductItem product={product} key={product.id} />;
+          })}
+        </section>
+        <ul className={s.pageNumbers}>
+          {pageNumbers.map(number => {
+            return (
+              <ProductListPage
+                key={number}
+                number={number}
+                currentCategory={currentCategory}
+              />
+            );
+          })}
+        </ul>
+      </React.Fragment>
+    );
+  }
 };
 
 export default withLoading(ProductListView);
